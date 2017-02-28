fbf1290c4eb09ba4b30b052b3c58fe50c094dba3
dinis brazão <dinis@torsk.co>
2017-02-28 00:05:06 +0000

    tidy up java code

--- app/src/main/java/org/montanha/nme/MainActivity.java
+++ app/src/main/java/org/montanha/nme/MainActivity.java
@@ -17,17 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void openBrowser(View view){
-
         //Get url from tag
         String url = (String)view.getTag();
-
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
         intent.addCategory(Intent.CATEGORY_BROWSABLE);
-
         //pass the url to intent data
         intent.setData(Uri.parse(url));
-
         startActivity(intent);
     }
 
